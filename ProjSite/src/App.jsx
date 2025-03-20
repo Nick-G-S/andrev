c7f574cc105c1df54446ba4cc5d1859dd1186505
Nicolas Gonçalves Sombrio <nicolassombro@gmail.com>
2025-03-19 21:16:09 -0300

    Update
    update

--- ProjSite/src/App.jsx
+++ ProjSite/src/App.jsx
@@ -6,11 +6,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
       <div className="app">
         <Header />
       </div>
-    </>
   );
 }
 
--- ProjSite/src/First_page/Header.jsx
+++ ProjSite/src/First_page/Header.jsx
@@ -9,8 +9,7 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import logo from '../assets/pichau-logo.png';
-
+import logo from "../assets/pichau-logo.png";
 
 import "../Css/header.css";
 
@@ -20,67 +19,87 @@ function Header() {
   return (
     <>
       <div className="home">
-        <span className="logo-container">
-          <img className="pichau-logo" src={logo} alt="Pichau-logo" />
-        </span>
-        <div className="departamentos">
-          <div className="dep-bars">
-            <FontAwesomeIcon icon={faBars} />
-          </div>
-          <div className="dep-words">
-            <span>ACESSE TODOS OS</span>
-            <span>DEPARTAMENTOS</span>
-            <FontAwesomeIcon icon={faAngleRight} />
-          </div>
-        </div>
-        <div className="options">
-          <div className="contAcess">
-            <FontAwesomeIcon icon={faCircleUser} />
-            <span>
-              Minha Conta  <strong>Entrar / Cadastro</strong>
-            </span>
-          </div>
-          <div className="contact">
-            <FontAwesomeIcon icon={faCircleQuestion} size="2x"/>
-            <span>
-             Atendimento
-             <br /> 
-             <strong>Ao cliente</strong>
-            </span>
-          </div>
-          <div className="favoritos">
-            <FontAwesomeIcon icon={faHeart} />
-            <span>
+        <header className="header-upper">
+          <span className="logo-container">
+            <img className="pichau-logo" src={logo} alt="Pichau-logo" />
+          </span>
+          <div className="options">
+            <div className="contAcess">
+              <button>
+                <FontAwesomeIcon icon={faCircleUser} color="red" />
+              </button>
               <span>
-                meus <strong>favoritos</strong>
+                Minha Conta
+                <br />
+                <strong>Entrar / Cadastro</strong>
               </span>
-            </span>
-          </div>
-          <div className="fundo">
-            <FontAwesomeIcon icon={faMoon} />
-            <span>
+            </div>
+            <div className="contact">
+              <button>
+                <FontAwesomeIcon icon={faCircleQuestion} color="red" />
+              </button>
               <span>
-                Modo <strong>Claro</strong>
+                Atendimento
+                <br />
+                <strong>Ao cliente</strong>
               </span>
-            </span>
-          </div>
-          {/* <div className="fundo">
+            </div>
+            <div className="favoritos">
+              <button>
+                <FontAwesomeIcon icon={faHeart} color="red" />
+              </button>
+              <span>
+                <span>
+                  meus
+                  <br />
+                  <strong>favoritos</strong>
+                </span>
+              </span>
+            </div>
+            <div className="fundo">
+              <button>
+                <FontAwesomeIcon icon={faMoon} color="red" />
+              </button>
+              <span>
+                Modo
+                <br />
+                <strong>Claro</strong>
+              </span>
+            </div>
+            {/* <div className="fundo">
             <FontAwesomeIcon icon={faSun} />
             <span>
               Modo<strong>Escuro</strong>
             </span>
           </div> */}
-          <div className="ShoppingCar">
-            <FontAwesomeIcon icon={faCartShopping} />
-            <span>
-              <strong>Carrinho</strong>
-            </span>
+            <div className="ShoppingCar">
+              <FontAwesomeIcon icon={faCartShopping} />
+              <span>
+                <strong>Carrinho</strong>
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="search">
-          <span>digite o que você procura...</span>
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </div>
+        </header>
+
+        <header className="header-under">
+          <button className="departamentos">
+            <div className="dep-bars">
+              <FontAwesomeIcon icon={faBars} size="2x" color="white" />
+            </div>
+            <div className="dep-words">
+              <div>ACESSE TODOS OS</div>
+              <strong>DEPARTAMENTOS</strong>
+            </div>
+            <div className="dep-arrow">
+              <FontAwesomeIcon icon={faAngleRight} color="white" />
+            </div>
+          </button>
+
+          <div className="search">
+            <span>digite o que você procura...</span>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
+        </header>
       </div>
     </>
   );
