def829b6e2d5326645acf45926febc695f035f8f
Nicolas Gonçalves Sombrio <nicolassombro@gmail.com>
2025-03-26 21:05:33 -0300

    update
    update

--- ProjSite/src/First_page/content.jsx
+++ ProjSite/src/First_page/content.jsx
@@ -4,6 +4,10 @@ import lil_promo from "../assets/promo-pichau-little.jpg";
 import lil_promo2 from "../assets/promo-pichau-little-2.jpg";
 import lil_wide_promo from "../assets/promo-pichau-little-wide.jpg";
 import lil_wide2_promo from "../assets/promo-pichau-little-wide-2.jpg";
+import gaming from "../assets/icon-gaming-white.png";
+import monte from "../assets/icon-monte-white.png";
+import pichau_white from "../assets/icon-pichau-white.png";
+import work_station from "../assets/icon-workstation.png";
 
 import "../Css/content.css";
 
@@ -32,6 +36,49 @@ function Content() {
 
         <div className="lil-wide promo">
           <img src={lil_wide2_promo} alt="" />
+        </div>
+      </div>
+
+      <div className="box-containers">
+        <div className="box">
+          <div className="box-logo">
+            <img src={gaming} alt="" />
+          </div>
+          <div className="box-txt">
+            <strong>GAMER</strong>
+            <span>O PC PERFEITO PARA JOGAR</span>
+          </div>
+        </div>
+
+        <div className="box">
+          <div className="box-logo">
+            <img src={work_station} alt="" />
+          </div>
+          <div className="box-txt">
+            <strong>WORK </strong>
+            <strong>STATION</strong>
+            <span>PARA MODELAGEM E RENDERIZAÇÃO</span>
+          </div>
+        </div>
+
+        <div className="box">
+          <div className="box-logo">
+            <img src={pichau_white} alt="" />
+          </div>
+          <div className="box-txt">
+            <strong>HOME</strong>
+            <span>PARA CASA OU ESCRITÓRIO</span>
+          </div>
+        </div>
+
+        <div className="box">
+          <div className="box-logo">
+            <img src={monte} alt="" />
+          </div>
+          <div className="box-txt">
+            <strong>MONTE</strong>
+            <span>O SEU PC</span>
+          </div>
         </div>
       </div>
     </div>
