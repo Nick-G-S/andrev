5eb1510f5f934ca9a5ae870155ed5f61da139a0c
Nicolas Gonçalves Sombrio <idontlovebhop@gmail.com>
2025-04-01 21:23:31 -0300

    Update
    Update

--- ProjSite/src/App.jsx
+++ ProjSite/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import Header from "./First_page/Header.jsx";
 import Content from "./First_page/Content.jsx";
+import Product from "./First_page/Product.jsx";
 import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-      <div className="app">
-        <Header />
-        <Content />
-      </div>
+    <div className="app">
+      <Header />
+      <Content />
+      <Product />
+
+    </div>
   );
 }
 
--- ProjSite/src/First_page/product.jsx
+++ ProjSite/src/First_page/product.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 
-import './product.css'
+import "../Css/product.css";
 
-function product() {
-  const [count, setCount] = useState(0)
+function Product() {
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="product">
+      <div className="new-product">
+        <div className="new-product-txt">
+          <FontAwesomeIcon icon={faCircleExclamation} size="2xl" color="red" />
+          <h1>PRODUTOS NOVOS E LANÇAMENTOS</h1>
+        </div>
+
+        <div className="product-box">
+          <div className="product-img">
+            <img src="" alt="" />
+          </div>
+          <div className="product-details">
+            <strong>
+              Gabinete Gamer Mancer Narok V2, Rainbow, Mid-Tower, Lateral de
+              Vidro, Com 3 Fans, Preto, MCR-NRK-V2
+            </strong>
+          </div>
+          <div className="product-price-vista">
+            <span>á vista 189,99</span>
+            <span>no pix com 15% de desconto</span>
+          </div>
+          <div className="separator">
+            
+          </div>
+          <div className="product-parcelado-inline">
+            <span>em até 12x de 185,90</span>
+            <span>sem juros no cartão</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default product
+export default Product;
