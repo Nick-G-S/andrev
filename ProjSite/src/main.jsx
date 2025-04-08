bdb9ef5e8c659a84f37bdfddd70d7bad7ffcd7d9
Nicolas Gonçalves Sombrio <idontlovebhop@gmail.com>
2025-04-07 21:10:56 -0300

    update
    mui and react route install, starting login page

--- ProjSite/src/main.jsx
+++ ProjSite/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import Login from './routes/Login.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App  />,
+  },
+  {
+    path: "/acount",
+    element: <Login />
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />
+);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
