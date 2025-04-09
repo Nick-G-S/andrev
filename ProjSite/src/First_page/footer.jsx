2126174c388f281f90552222f09ed60471dc6e91
Andre07820 <andrecarlos07820@gmail.com>
2025-04-08 22:10:14 -0300

    08/04/2025

--- ProjSite/src/First_page/footer.jsx
+++ ProjSite/src/First_page/footer.jsx
@@ -4,6 +4,14 @@ import { faClock } from "@fortawesome/free-regular-svg-icons";
 import { faPhoneVolume } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import Pichau_Empresas from "../assets/Pichau_Empresas.png";
+import meio_pagamento1 from "../assets/meio_pagamento1.png";
+import meio_pagamento2 from "../assets/meio_pagamento2.png";
+import meio_pagamento3 from "../assets/meio_pagamento3.png";
+import meio_pagamento4 from "../assets/meio_pagamento4.png";
+import meio_pagamento5 from "../assets/meio_pagamento5.png";
+import meio_pagamento6 from "../assets/meio_pagamento6.png";
+
 
 
 
@@ -52,42 +60,46 @@ function footer() {
             <strong>(47) 3305-5150</strong>
           </div>
           <div className="sac">
-          <div className="FontAwesomeIconicon">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </div>
+            <div className="FontAwesomeIconicon">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </div>
             <strong>sac@pichau.com.br</strong>
           </div>
         </div>
 
 
         <div className="atendimento_fisico">
-        <div className="FontAwesomeIconicon">
-        <FontAwesomeIcon icon={faHouse} />
-            </div>
-          <strong>loja fisica 
+          <div className="FontAwesomeIconicon">
+            <FontAwesomeIcon icon={faHouse} />
+          </div>
+          <strong>loja fisica
             <br />
             em joinville</strong>
-            <div class="separacao"></div>
-            <span>
-          <strong>(47)3026-6446 </strong>
-          <br />
-          segunda a sexta das 9h as 19h 
-          <br />
-           sabado das 9h as 13H
+          <div class="separacao"></div>
+          <span>
+            <strong>(47)3026-6446 </strong>
+            <br />
+            segunda a sexta das 9h as 19h
+            <br />
+            sabado das 9h as 13H
 
           </span>
         </div>
       </div>
 
-      
-      <span>banner</span>
+      <div className="empresas">
+        <a href=""><img src={Pichau_Empresas} alt="" /></a>
+      </div>
 
-      <div className="informativos">
+
+      <div className="footerLiks">
 
         <div className="praticos">
           <div className="intitucional">
-            <p>nossas lojas</p>
-            <li> <a href="">quem somos</a></li>
+            <p>institucional
+            </p>
+
+            <li><a href="">quem somos</a></li>
             <li><a href="">localização </a></li>
             <li><a href="">nossas lojas </a></li>
             <li><a href="">blog </a></li>
@@ -108,35 +120,40 @@ function footer() {
             <li><a href="">politicas de privacidade </a></li>
           </div>
         </div>
-      </div>
-      <div className="meiosPagamento">
-        <p>pagamento</p>
-        <div className="FTmeiospagamento">
-          <li>deve ter fotos dos meios de pagamento</li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </div>
-      </div>
 
-      <div className="Informativos">
-        <p>informativos</p>
-        <strong>receba ofertas exclusivas em seu email</strong>
-        <div>
-          insira seu email
-        </div>
-        <div className="RedesSociais">
-          <span>siga-nos nas redes sociais</span>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
-        </div>
-        <p>baixe os aplicativos</p>
-        <a href=""></a>
-        <a href=""></a>
-      </div>
 
+
+
+        <div className="meiosPagamento">
+          <p>pagamento</p>
+          <div className="FTmeiospagamento">
+            <li><img src={meio_pagamento1} /></li>
+            <li><img src={meio_pagamento2} /></li>
+            <li><img src={meio_pagamento3} /></li>
+            <li><img src={meio_pagamento4} /></li>
+            <li><img src={meio_pagamento5} /></li>
+            <li><img src={meio_pagamento6} /></li>
+          </div>
+        </div>
+
+        <div className="Informativos">
+          <p>informativos</p>
+          <strong>receba ofertas exclusivas em seu email</strong>
+          <div>
+            insira seu email
+          </div>
+          <div className="RedesSociais">
+            <span>siga-nos nas redes sociais</span>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+          </div>
+          <p>baixe os aplicativos</p>
+          <a href=""></a>
+          <a href=""></a>
+        </div>
+      </div>
 
     </>
   )
